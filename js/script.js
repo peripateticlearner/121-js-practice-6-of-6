0c46cb48b407b8f9371f99d3999fe8a0b5151199
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-10-12 14:20:49 -0700

    Update script.js
    I changed the event to toggle on and off different classes (i.e. background colors) based on if the user is feeling happy, sad, or passionate.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var moodSelect = document.querySelector(".mood-select");
+//console.log(moodSelect);
+var body = document.querySelector("body");
+
+moodSelect.addEventListener("change", function (e) {
+  var mood = e.target.value;
+  if (mood === "happy") {
+    body.classList.remove("sad");
+    body.classList.remove("passionate");
+    body.classList.add("happy");
+  } else if (mood === "sad") {
+    body.classList.remove("happy");
+    body.classList.remove("passionate");
+    body.classList.add("sad");
+  } else if (mood === "passionate") {
+    body.classList.remove("sad");
+    body.classList.remove("happy");
+    body.classList.add("passionate");
+  }
+});
